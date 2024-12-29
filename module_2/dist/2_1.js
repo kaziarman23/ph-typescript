bbe478c651e5f500f9a821e2f7c43143c13b2507
Ethernal Knight <kaziarman@proton.me>
2024-12-29 12:47:39 +0600

    added module_2

---
+++ module_2/dist/2_1.js
@@ -0,0 +1,22 @@
+"use strict";
+{
+    // type assertion
+    // It's decleared a key word: "as" to defined the type of a spacifike data.
+    const kgToGram = (value) => {
+        if (typeof value === "string") {
+            return parseInt(value) * 1000;
+        }
+        else if (typeof value === "number") {
+            return value * 1000;
+        }
+        else {
+            return "Invaild Input";
+        }
+    };
+    const result_1 = kgToGram("1");
+    const result_2 = kgToGram(10);
+    console.log({ result_1, result_2 });
+    console.log(typeof result_1);
+    console.log(typeof result_2);
+    // Scope Ending
+}
---
+++ module_2/src/2_1.ts
@@ -0,0 +1,20 @@
+{
+  const kgToGram = (value: string | number) => {
+    if (typeof value === "string") {
+      return parseInt(value) * 1000;
+    } else if (typeof value === "number") {
+      return value * 1000;
+    } else {
+      return "Invaild Input";
+    }
+  };
+
+  const result_1 = kgToGram("1");
+  const result_2 = kgToGram(10);
+
+  console.log({ result_1, result_2 });
+  console.log(typeof result_1);
+  console.log(typeof result_2);
+
+  // Scope Ending
+}
